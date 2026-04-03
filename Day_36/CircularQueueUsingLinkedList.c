a4fe73b8c85c837d05d58a7a146aa18e206fbfff
1234abhibisht <abhibisht129@gmail.com>
2026-04-03 13:01:55 +0530

    Update CircularQueueUsingLinkedList.c

--- Day_36/CircularQueueUsingLinkedList.c
+++ Day_36/CircularQueueUsingLinkedList.c
@@ -29,4 +29,36 @@ void enqueue(int value) {
         rear = newNode;
         rear->next = front;      // maintain circular property
     }
+    void display() {
+    if (front == NULL) {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    struct Node* temp = front;
+
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != front);
+
+    printf("\n");
+}
+int main() {
+    int n, value;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter value: ");
+        scanf("%d", &value);
+        enqueue(value);
+    }
+
+    printf("Circular Queue: ");
+    display();
+
+    return 0;
+}
 }
