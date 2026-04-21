014d9cde36d25007b94eee003fb6f1accc647e78
1234abhibisht <abhibisht129@gmail.com>
2026-04-21 23:15:41 +0530

    Add binary tree construction from preorder and inorder
    Implement a function to build a binary tree from preorder and inorder traversals and perform postorder traversal.

---
+++ Day_58/BuildTreeFromInorderAndPreorder.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Find index in inorder
+int search(int inorder[], int start, int end, int value) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+// Build tree
+Node* buildTree(int preorder[], int inorder[], int start, int end, int* preIndex) {
+    if (start > end)
+        return NULL;
+
+    int rootVal = preorder[*preIndex];
+    (*preIndex)++;
+
+    Node* root = createNode(rootVal);
+
+    if (start == end)
+        return root;
+
+    int inIndex = search(inorder, start, end, rootVal);
+
+    root->left = buildTree(preorder, inorder, start, inIndex - 1, preIndex);
+    root->right = buildTree(preorder, inorder, inIndex + 1, end, preIndex);
+
+    return root;
+}
+
+// Postorder traversal
+void postorder(Node* root) {
+    if (root == NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int preorder[n], inorder[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &preorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    int preIndex = 0;
+    Node* root = buildTree(preorder, inorder, 0, n - 1, &preIndex);
+
+    postorder(root);
+
+    return 0;
+}
